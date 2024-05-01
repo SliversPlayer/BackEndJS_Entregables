08e9b43291c47fc2ef0cfa4b7522017f83002f9a
Roberto Méndez Strobl <rmendezstrobl@gmail.com>
2024-05-01 16:00:08 -0400

    correccion al codigo

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import handlebars from 'express-handlebars'
 import productManager from './src/PreEntrega1.js';
 import cartManager from './src/PreEntrega1_cart.js';
+import __dirname from './utils.js'
 
 const app = express();
 const PORT = 8080;
+
+//Inicializar motor usando app.engine
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname+'views');
+
+app.set('views engine','handlebars');
 
 // Middleware para manejar JSON
 app.use(express.json());
